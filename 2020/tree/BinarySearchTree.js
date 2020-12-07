f13a3da9ddf27b3c7b7d43e882f6fa3db77cdfde
shihuan <745935253@qq.com>
2020-12-07 20:36:24 +0800

    feature leetcode for tree

--- 2020/tree/BinarySearchTree.js
+++ 2020/tree/BinarySearchTree.js
@@ -356,3 +356,139 @@ avlTree.insert(80)
 avlTree.insert(65)
 
 console.log('avlTree', avlTree)
+
+const Colors = {
+    BLACK: 'black',
+    RED: 'red',
+}
+class RedBlackNode extends Node { 
+    constructor(key) { 
+        super(key); 
+        this.key = key; 
+        this.color = Colors.RED; // {6} 
+        this.parent = null; // {7} 
+    } 
+    isRed() { 
+        return this.color === Colors.RED; 
+    } 
+}
+class RedBlackTree extends BinarySearchTree{
+    constructor(){
+        this.root = null
+    }
+    insert(key){
+        if(this.root == null){
+            this.root = new RedBlackNode(key)
+            this.root.color = Colors.BLACK
+        } else {
+            const newNode = this.insertNode(this.root, key)
+            this.fixTreeProperties(newNode)
+        }
+    }
+    insertNode(node, key){
+        if(key < node.key){
+            if(node.left == null){
+                node.left = new RedBlackNode(key)
+                node.left.parent = node
+                return node.left
+            } else {
+                return this.insertNode(node.left, key)
+            }
+        } else {
+            if(node.right == null){
+                node.right = new RedBlackNode(key)
+                node.right.parent = node
+                return node.right
+            } else {
+                return this.insertNode(node.right, key)
+            }
+        }
+    }
+    rotationLL(node){
+        const tmp = node.left
+        node.left = tmp.right
+        if(tmp.right && tmp.right.key){
+            tmp.right.parent = node
+        }
+        tmp.parent = node.parent
+        if(node.parent === null){
+            this.root = tmp
+        } else {
+            if(node === node.parent.left){
+                node.parent.left = tmp
+            } else {
+                node.parent.right = tmp
+            }
+        }
+        tmp.right = node
+        node.parent = tmp
+    }
+    rotationRR(node){
+        const tmp = node.right
+        node.right =tmp.left
+        if(tmp.left && tmp.left.key){
+            tmp.left.parent = node
+        }
+        // tmp的父节点，判断node是否是根节点
+        tmp.parent = node.parent
+        if(tmp.parent == null){
+            this.root = tmp
+        }else{
+            if(node === node.parent.left){
+                node.parent.left = tmp
+            } else{
+                node.parent.right = tmp
+            }
+        }
+        // tmp 左节点，及左节点的父节点
+        tmp.left = node
+        node.parent = tmp
+    }
+    fixTreeProperties(node){
+        while(node && node.parent && node.parent.isRed()
+                && node.isRed()
+        ){
+            const parent = node.parent
+            const grandParent = node.parent.parent
+            if(grandParent && grandParent.left === parent){
+                const uncle = grandParent.right
+                if(uncle && uncle.isRed()){
+                    grandParent.color = Colors.RED
+                    parent.color = Colors.BLACK
+                    parent.color = Colors.BLACK
+                    ndoe = grandParent
+                }else{
+                    if(node === parent.right){
+                        this.rotationRR(parent)
+                        node = parent
+                        parent = node.parent
+                    }
+                    this.rotationLL(parent)
+                    parent.color = Colors.BLACK
+                    grandParent.color = Colors.RED
+                    node = parent
+                }
+            } else {
+                const uncle = grandParent.left
+                if(uncle && uncle.isRed()){
+                    grandParent.color = Colors.RED
+                    parent.color = Colors.BLACK
+                    parent.color = Colors.BLACK
+                    ndoe = grandParent
+                }else{
+                    if(node === parent.left){
+                        this.rotationLL(parent)
+                        node = parent
+                        parent = node.parent
+                    }
+                    this.rotationRR(parent)
+                    grandParent.color = Colors.RED
+                    parent.color = Colors.BLACK
+                    node = parent
+                }
+            }
+        }
+        this.root.color = Colors.BLACK
+    }
+}
+
