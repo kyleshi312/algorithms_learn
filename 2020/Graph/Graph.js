250a50f2c03ced1eecca71a8ba9d2093c7fe46d2
shihuan <745935253@qq.com>
2021-01-06 18:58:24 +0800

    feature BreadthFirstSearch

---
+++ 2020/Graph/Graph.js
@@ -0,0 +1,72 @@
+class Graph{
+    constructor(isDirected){
+        // isDirected: indicate if the graph is directed,defaultly is not direct 
+        this.isDirected = isDirected
+        // save vertexes
+        this.vertices = []
+        // save linked table
+        this.adjList = new Dictionary()
+    }
+    addVertex(v){
+        if(!this.vertices.includes(v)){
+            this.vertices.push(v)
+            this.adjList.set(v, [])
+        }
+    }
+    addEdge(v, w){
+        // init the vertex in the vertices
+        // init dictionary
+        if(!this.adjList.get(v)){
+            this.addVertex(v)
+        }
+        if(!this.adjList.get(w)){
+            this.addVertex(w)
+        }
+        this.adjList.get(v).push(w)
+        // if not directed, two vertex is as 
+        if(!this.isDirected){
+            this.adjList.get(w).push(v)
+        }
+    }
+    getVertices(){
+        return this.vertices
+    }
+    getAdjList(){
+        return this.adjList
+    }
+    toString(){
+        let s = ''
+        for(let i=0;i<this.vertices.length;i++){
+            let vertex = this.vertices[i]
+            s += `${vertex} => `
+            let neg = this.adjList.get(vertex)
+            for(let j=0;j<neg.length;j++){
+                s += `${neg[j]} `
+            }
+            s += '\n'
+        }
+        return s
+    }
+}
+
+
+const graph = new Graph(); 
+const myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']; // {12} 
+for (let i = 0; i < myVertices.length; i++) { // {13} 
+ graph.addVertex(myVertices[i]); 
+} 
+graph.addEdge('A', 'B'); 
+// {14} 218 第 12 章 图
+graph.addEdge('A', 'C'); 
+graph.addEdge('A', 'D'); 
+graph.addEdge('C', 'D'); 
+graph.addEdge('C', 'G'); 
+graph.addEdge('D', 'G'); 
+graph.addEdge('D', 'H'); 
+graph.addEdge('B', 'E'); 
+graph.addEdge('B', 'F'); 
+graph.addEdge('E', 'I');
+
+console.log('graph', JSON.parse(JSON.stringify(graph)))
+console.log('graph tostring', graph.toString())
+
---
+++ 2020/Graph/searchFunc.js
@@ -0,0 +1,107 @@
+// 颜色对象
+const Colors = { 
+    WHITE: 0, 
+    GREY: 1, 
+    BLACK: 2 
+   };
+
+// 颜色标记方法
+const initializeColor = vertices => { 
+    const color = {}; 
+    for (let i = 0; i < vertices.length; i++) { 
+    color[vertices[i]] = Colors.WHITE; 
+    } 
+    return color; 
+};
+
+// 广度优先搜索
+// 1.创建一个队列Q
+// 2.标注v为被发现的，加入队列！Q
+// 3.如果Q非空，则运行以下步骤：
+    // a 将u出队列
+    // b 标记为已访问（灰色）
+    // c 将所有u的未访问邻点都放入Q
+    // d 标注u为已被探索的（黑色）
+const breadthFirstSearch = (graph, startVertex, callback) => {
+    const vertices = graph.getVertices()
+    const adjList = graph.getAdjList()
+    const color = initializeColor(vertices)
+    const queue = new Queue()
+    queue.enqueue(startVertex)
+    while(queue.size()){
+        const u = queue.dequeue()
+        // console.log('u', u)
+        // console.log('adjList', adjList)
+        const vers = adjList.get(u)
+        color[u] = Colors.GREY
+        for(let i=0; i<vers.length;i++){
+            if(color[vers[i]] === Colors.WHITE){
+                queue.enqueue(vers[i])
+                color[vers[i]] = Colors.GREY
+            }
+        }
+        color[u] = Colors.BLACK
+        if(callback){
+            callback(u)
+        }
+    }
+}
+const printVertex = (value) => console.log('Visited vertex: ' + value); // {15} 
+breadthFirstSearch(graph, myVertices[0], printVertex);
+
+// 使用BFS查找最短路径
+const BFS = (graph, startVertex,) => {
+    const vertices = graph.getVertices()
+    const adjList = graph.getAdjList()
+    const color = initializeColor(vertices)
+    const distances = {}
+    const predecessors = {}
+    for(let n of vertices){
+        distances[n] = 0
+        predecessors[n] = null
+    }
+    const queue = new Queue()
+    queue.enqueue(startVertex)
+    color[startVertex] = Colors.GREY
+    while(!queue.isEmpty()){
+        const u = queue.dequeue()
+        const neighbors = adjList.get(u)
+        for(let n of neighbors){
+            // console.log('n', n)
+            // console.log('color[n]', color[n])
+            if(color[n] === Colors.WHITE){
+                color[n] = Colors.GREY
+                distances[n] = distances[u] + 1
+                predecessors[n] = u
+                queue.enqueue(n)
+            }
+        }
+        color[u] = Colors.BLACK
+    }
+    return {
+        distances,
+        predecessors
+    }
+}
+
+const shortestPathA = BFS(graph, myVertices[0])
+console.log('shortestPathA', shortestPathA)
+// const myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']; // {12} 
+// distances: [A: 0, B: 1, C: 1, D: 1, E: 2, F: 2, G: 2, H: 2 , I: 3], 
+// predecessors: [A: null, B: "A", C: "A", D: "A", E: "B", F: "B", G: "C", H: "D", I: "E"]
+// const fromVertex = myVertices[0]; // {9} 
+for (i = 1; i < myVertices.length; i++) { // {10} 
+    const toVertex = myVertices[i]; // {11} 
+    const path = new Stack(); // {12} 
+    for (let v = toVertex; 
+        v !== fromVertex; 
+        v = shortestPathA.predecessors[v]) { // {13} 
+        path.push(v); // {14} 
+    } 
+    path.push(fromVertex); // {15} 
+    let s = path.pop(); // {16} 
+    while (!path.isEmpty()) { // {17} 
+        s += ' - ' + path.pop(); // {18} 
+    } 
+    console.log(s); // {19} 
+}
